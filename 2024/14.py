6f93c06750286ac6821e97b50b59051e8ad23cfd
Merel <164741156+MerelVanEssen@users.noreply.github.com>
2024-12-14 11:41:28 +0100

    Update 14.py ready

--- 2024/14.py
+++ 2024/14.py
@@ -1,102 +1,104 @@
 import re
-from collections import deque, Counter, defaultdict
-from functools import reduce
+import functools
 
 class Solution:
 	def __init__(self, input, x, y):
-		self.found = 0
-		self.saveField = []
-		self.map = input.split('\n')
+		pattren = r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)"
+		match = re.findall(pattren, input)
 		self.lenY = y
 		self.lenX = x
-		field = ['.' for _ in range(x)]
-		self.field = [field[:] for _ in range(y)]
-		for i in range(len(self.map)):
-			parts = self.map[i].split()
-			part1 = parts[0].split(',')
-			part2 = parts[1].split(',')
-			part1 = [int(x) for x in part1]
-			part2 = [int(x) for x in part2]
-			if part2[0] < 0:
-				part2[0] = -part2[0]
-				part2[0] %= self.lenX
-				part2[0] = -part2[0]
-			else:
-				part2[0] %= self.lenX
-			if part2[1] < 0:
-				part2[1] = -part2[1]
-				part2[1] %= self.lenY
-				part2[1] = -part2[1]
-			else:
-				part2[1] %= self.lenY
-			self.map[i] = [part1,part2]
+		self.robots = []
+		self.robots2 = []
+		self.saveRobotsFromLine = set()
 
+		# Saves the nrs and calculates the modus of each move coordinate
+		for m in match:
+			moveX = int(m[2])
+			if moveX < 0:
+				moveX = -moveX % self.lenX
+				moveX = -moveX
+			else:
+				moveX %= self.lenX
+			moveY = int(m[3])
+			if moveY < 0:
+				moveY = -moveY % self.lenY
+				moveY = -moveY
+			else:
+				moveY %= self.lenY
+			self.robots.append([int(m[0]), int(m[1]), moveX, moveY])
+			self.robots2.append([int(m[0]), int(m[1]), moveX, moveY])
+
+	# if the new place is outsides the field it replaces it back in the field
+	@cache
 	def corrector(self, newPos, length):
 		if newPos >= length:
-			newPos = newPos % length
+			return newPos % length
 		elif newPos < 0:
-			newPos = length - abs(newPos)
+			return length - abs(newPos)
 		return newPos
-
 	
-	def searchTree(self, field, y, x):
-		lineLen = 0
-		while y + lineLen < self.lenY and field[y + lineLen][x] == '#':
-			lineLen += 1		
-		return lineLen
+	# Search after the longest line that resembles the trunk of the christmas tree
+	def searchTrunkLength(self, field, y, x):
+		self.saveRobotsFromLine.add((y, x))
+		trunkLength = 0
+		while y + trunkLength < self.lenY and field[y + trunkLength][x] == '#':
+			trunkLength += 1		
+		return trunkLength
 
 	def part1(self):
-		total = 0
-		field = ['.' for _ in range(self.lenX)]
-		field = [field[:] for _ in range(self.lenY)]
-		saveMap = []
-		maxLinefield = 0
-		saveTimes = 0
-		for times in range(50000):
-			field = ['.' for _ in range(self.lenX)]
-			field = [field[:] for _ in range(self.lenY)]
-			for i in range(len(self.map)):
-				pos, move = self.map[i]
-				pos[0] = self.corrector(pos[0] + move[0], self.lenX)
-				pos[1] = self.corrector(pos[1] + move[1], self.lenY)
-				self.map[i] = [pos, move]
-				field[pos[1]][pos[0]] = '#'
-			for i in range(len(self.map)):
-				lineleng = self.searchTree(field, self.map[i][0][1],self.map[i][0][0])
-				if maxLinefield < lineleng:
-					saveTimes = times + 1
-					maxLinefield = lineleng
-					# saveField = field[:]
-					# saveMap = self.map[:]
+		for times in range(100):
+			for i in range(len(self.robots)):
+				posX, posY, moveX, moveY = self.robots[i]
+				posX = self.corrector(posX + moveX, self.lenX)
+				posY = self.corrector(posY + moveY, self.lenY)
+				self.robots[i] = [posX, posY, moveX, moveY]
+		
+		# Checks where the robot is and devide them in the 4 parts
 		middleX = self.lenX // 2
 		middleY = self.lenY // 2
-		middle = 0
 		totalQ = [0,0,0,0]
-		for pos, move in self.map:
-			if pos[0] == middleX or pos[1] == middleY:
-				middle += 1
-				continue
-			if pos[0] < middleX and pos[1] < middleY:
+		for posX, posY, moveX, moveY in self.robots:
+			if posX < middleX and posY < middleY:
 				totalQ[0] += 1
-			elif pos[0] > middleX and pos[1] < middleY:
+			elif posX > middleX and posY < middleY:
 				totalQ[1] += 1
-			elif pos[0] < middleX and pos[1] > middleY:
+			elif posX < middleX and posY > middleY:
 				totalQ[2] += 1
-			elif pos[0] > middleX and pos[1] > middleY:
+			elif posX > middleX and posY > middleY:
 				totalQ[3] += 1
-		return totalQ[0] * totalQ[1] * totalQ[2] * totalQ[3], saveTimes
+		return totalQ[0] * totalQ[1] * totalQ[2] * totalQ[3]
+
+	def part2(self):
+		fieldline = ['.' for _ in range(self.lenX)]
+		currField = [fieldline[:] for _ in range(self.lenY)]
+		maxTrunkLength = 0
+		saveTimes = 0
+		for times in range(10000):	
+			currField = [fieldline[:] for _ in range(self.lenY)]
+			for i in range(len(self.robots2)):
+				posX, posY, moveX, moveY = self.robots2[i]
+				posX = self.corrector(posX + moveX, self.lenX)
+				posY = self.corrector(posY + moveY, self.lenY)
+				self.robots2[i] = [posX, posY, moveX, moveY]
+				currField[posY][posX] = '#'
+
+			# Checks for each robot if there is a line from that point
+			for i in range(len(self.robots2)):
+				if (self.robots2[i][1],self.robots2[i][0]) in self.saveRobotsFromLine and self.lenY - self.robots2[i][1] < trunkLength:
+					continue
+				trunkLength = self.searchTrunkLength(currField, self.robots2[i][1],self.robots2[i][0])
+				if maxTrunkLength < trunkLength:
+					saveTimes = times + 1
+					maxTrunkLength = trunkLength
+			self.saveRobotsFromLine.clear()
+		return saveTimes
 
 def main():
-	# CHANGE INPUTFILE
 	input = open("input.txt", "r").read()
 
-	# sol = Solution(input, 11, 7)
 	sol = Solution(input, 101, 103)
-	print("Part 1 TEST:", sol.part1())
+	print("Part 1:", sol.part1())
+	print("Part 2:", sol.part2())
 
 if __name__ == "__main__":
 	main()
-
-	# 50331600 too low
-	# 224446464 too low
-	# 227002500 too low
