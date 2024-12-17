bc760cf374eccb7471fd80094571fe4a292e1f49
Merel <mvan-ess@codam.student.nl>
2024-12-17 15:57:12 +0100

    day17 2023

--- 2023/17notready.py
+++ 2023/17notready.py
@@ -13,62 +13,53 @@ import heapq
 class Solution:
 	def __init__(self, input):
 		self.map = input.split('\n')
-		self.lX = len(self.map)
-		self.lY = len(self.map[0])
-		self.dir = [[0,1], [1,0], [0,-1], [-1,0]]
-		self.contraDir = [2,3,0,1]
-		self.grid = [[float('inf') for _ in range(self.lY)] for _ in range(self.lX)]
+		self.lY = len(self.map)
+		self.lX = len(self.map[0])
+		self.dir = [(0,1), (1,0), (0,-1), (-1,0)]
+		self.grid = [[float('inf') for _ in range(self.lX)] for _ in range(self.lY)]
+		self.start = (0,0)
+		self.end = (self.lY - 1, self.lX - 1)
 
 	def dijkstra(self):
 		visited = set()
 		deq = []
-		heapq.heappush(deq, (0, 0, 0, 1, 0)) # cost, y, x, steps, dir left
-		heapq.heappush(deq, (0, 0, 0, 1, 1)) # cost, y, x, steps, dir down
+		heapq.heappush(deq, (0, 0, 0, False, 0, 1)) # cost, y, x, steps, dir left
+		heapq.heappush(deq, (0, 0, 0, False, 1, 0)) # cost, y, x, steps, dir down
 		self.grid[0][0] = 0
 
 		while deq:
-			cost, y, x, straigth, direction = heapq.heappop(deq)
+			cost, y, x, turn, dY, dX = heapq.heappop(deq)
 
-			if (y, x, direction) in visited:
+			if (y, x) == self.end:
+				return cost
+
+			if (y, x, dY, dX) in visited:
 				continue
 
-			for i, d in enumerate(self.dir):
+			visited.add((y, x, dY, dX))
 
+			# print(y, x, turn, dY, dX, "cost", cost)
+
+			if turn == False:
 				newCost = cost
-				newY = y + d[1]
-				newX = x + d[0]
+				for i in range(3):
+					y += dY
+					x += dX
+					
+					if 0 <= y < self.lY and 0 <= x < self.lX:
+						newCost += int(self.map[y][x])
+						# print(y, x, newCost)
+						heapq.heappush(deq, (newCost, y, x, True,  dY, dX))
+			else:
+				for dirY, dirX in {(-dX, dY), (dX, -dY)}:
+					if (y, x, False, dirY, dirX) not in visited:
+						heapq.heappush(deq, (cost, y, x, False, dirY, dirX))
 
-				if not (0 <= newY < len(self.map) and 0 <= newX < len(self.map[0])):
-					continue
-
-				if direction == i and straigth == 3:
-					continue
-
-				newCost = cost + int(self.map[newY][newX])
-
-				if y == len(self.map) - 1 and x == len(self.map[0]) - 1:
-					return (newCost)
-
-				if straigth == 4:
-					straigth = 1
-				else:
-					straigth += 1
-
-				if newCost < self.grid[newY][newX]:
-					self.grid[newY][newX] = newCost
-
-				heapq.heappush(deq, (newCost, newY, newX, straigth, i))
-
-			visited.add((y, x, direction))
-
-		return self.grid[len(self.map) - 1][len(self.map[0]) - 1]
+		return 0
 					
 
 	def part1(self):
 		total = self.dijkstra()
-
-		for line in self.grid:
-			print(line)
 		return total
 
 	def part2(self):
@@ -81,7 +72,7 @@ def main():
 	input = open("input/17.txt", "r").read()
 
 	sol = Solution(input)
-	print("Part 1:", sol.part1())
+	print("Part 1:", sol.part1(), 936)
 	print("Part 2:", sol.part2())
 
 if __name__ == "__main__":
