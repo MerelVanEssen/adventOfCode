d6de1b8f7d75d2c4535bd61e65217e55a899e8dd
Merel <mvanessen1990@gmail.com>
2024-12-18 20:24:30 +0100

    day 18 2023

--- 2023/18.py
+++ 2023/18.py
@@ -4,96 +4,75 @@ from math import gcd
 from functools import reduce
 from collections import Counter
 from aoc import turnMapBackwardsList
+import heapq
 
 # self.hands = [[] for _ in range(7)]
 # inte = [int(x) for x in line]
 # self.field = [['.' for _ in row] for row in self.map]
 
+# four corners UL UR DL DR
+
 class Solution:
 	def __init__(self, input):
-		pattern = r"([A-Z]) ([\d+]) \((.*)\)"
+		pattern = r"([A-Z]) (\d+) \(#(.*)\)"
 		self.matches = re.findall(pattern, input)
-		self.d = {"R": [0,1], 'L': [0,-1], "U": [-1,0], "D": [1,0]}
-		self.map = []
+		d = {"R": [0,1], 'L': [0,-1], "U": [-1,0], "D": [1,0]}
+		self.points = [[0,0]]
+		self.b = 0
 
-	def searchLowest(self, way):
-		lowestY = float('inf')
-		lowestX = float('inf')
-		highestY = float('-inf')
-		highestX = float('-inf')
-
-		for y, x in way:
-			lowestY = min(lowestY, y)
-			lowestX = min(lowestX, x)
-			highestY = max(highestY, y)
-			highestX = max(highestX, x)
-		return abs(lowestY) + 3, abs(lowestX) + 3, highestY + abs(lowestY) + 6, highestX + abs(lowestX) + 6
-
-	def drawMap(self, way, correctionY, correctionX, maxY, maxX):
-		self.map = [['.' for _ in range(maxX + 1)] for _ in range(maxY + 1)]
-		y = way[0][0]
-		x = way[0][1]
-		for y, x in way:
-			self.map[y + correctionY][x + correctionX] = '#'
+		y = x = 0
+		for direction, steps, color in self.matches:
+			dy, dx = d[direction]
+			steps = int(steps)
+			self.b += steps
+			y += (dy * steps)
+			x += (dx * steps)
+			self.points.append([y, x])
 	
-	def fillMap(self):
-		outside = True
-		deq = []
+	def hexaConverter(self, color):
+		hexadecimal = "0123456789abcdef"
 
-		for i, line in enumerate(self.map):
-			if line.count('#') == 2:
-				inside = True
-				savePoints = set()
-				index = line.index('#')
-				deq.append([i, index + 1])
-				savePoints.add((i, index + 1))
-				while deq:
-					y, x = deq.pop()
-					for dY, dX in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
-						yNew = y + dY
-						xNew = x + dX
-						if not (0 <= yNew < len(self.map) and 0 <= xNew < len(self.map[0])):
-							inside = False
-							break
-						if self.map[yNew][xNew] == '.' and (yNew, xNew) not in savePoints:
-							deq.append([yNew, xNew])
-							savePoints.add((yNew, xNew))
-				if inside == True:
-					for y,x in savePoints:
-						self.map[y][x] = '#'
-
-		for line in self.map:
-			print(line)
+		nr = 0
+		for i, hexa in enumerate(reversed(color)):
+			value = hexadecimal.index(hexa)
+			nr += value * (16 ** i)  
+		return nr
 
 	def part1(self):
-		d = {"R": [0,1], 'L': [0,-1], "U": [-1,0], "D": [1,0]}
-		y = x = 0
-		way = set()
-		for match in self.matches:
-			step = 0
-			while step < int(match[1]):
-				y += self.d[match[0]][0]
-				x += self.d[match[0]][1]
-				step += 1
-				way.add((y, x))
-		correctionY, correctionX, maxY, maxX = self.searchLowest(list(way))
-		print(way)
-		print(correctionY, correctionX, maxY, maxX)
-		self.drawMap(list(way), correctionY, correctionX, maxY, maxX)
-		self.fillMap()
+		boundary = self.b
+		area = 0
+		# Shoelace formula
+		for i, point in enumerate(self.points):
+			y, x = point[0], point[1]
+			if i + 1 == len(self.points):
+				area += y * (self.points[i - 1][1] - self.points[-1][1])
+			else:
+				area += y * (self.points[i - 1][1] - self.points[i + 1][1])
+		area = abs(area) / 2
 
-		total = 0
-		for line in self.map:
-			total += line.count('#')
-		return total
+		# Pick's theorem
+		insideArea = area - (boundary / 2) + 1
+		return int(insideArea + boundary)
 
 	def part2(self):
-		total = 0
+		directions = [[0,1],[1,0],[0,-1],[-1,0]]
+		self.points.clear()
+		self.points = [[0,0]]
 
-		return total
+		y = x = 0
+		self.b = 0
+		for d, s, color in self.matches:
+			distance = self.hexaConverter(color[:5])
+			direction = self.hexaConverter(color[5])
+
+			dy, dx = directions[direction]
+			self.b += distance
+			y += dy * distance
+			x += dx * distance
+			self.points.append([y, x])
+		return self.part1()
 
 def main():
-	# CHANGE INPUTFILE
 	input = open("input/18.txt", "r").read()
 
 	sol = Solution(input)
