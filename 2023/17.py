27429e0e5bedb73faf8752e91c679c66eedbda5d
Merel <mvan-ess@codam.student.nl>
2024-12-18 10:39:45 +0100

    day 17 2023 ready

--- 2023/17.py
+++ 2023/17.py
@@ -1,14 +1,4 @@
-import re
-from collections import deque
-from math import gcd
-from functools import reduce
-from collections import Counter
-from aoc import turnMapBackwardsList
 import heapq
-
-# self.hands = [[] for _ in range(7)]
-# inte = [int(x) for x in line]
-# self.field = [['.' for _ in row] for row in self.map]
 
 class Solution:
 	def __init__(self, input):
@@ -16,9 +6,8 @@ class Solution:
 		self.lY = len(self.map)
 		self.lX = len(self.map[0])
 		self.dir = [[0,1], [1,0], [0,-1], [-1,0]]
-		self.grid = [[float('inf') for _ in range(self.lX)] for _ in range(self.lY)]
 
-	def dijkstra(self):
+	def part1_dijkstra(self):
 		visited = set()
 		deq = []
 		heapq.heappush(deq, (0, 0, 0, 1, 0, 0)) # cost, y, x, steps, dirY, dirX
@@ -33,6 +22,7 @@ class Solution:
 				continue
 			visited.add((y, x, dy, dx, s))
 			
+			# found end
 			if y == self.lY - 1  and x == self.lX - 1:
 				return cost
 			
@@ -52,29 +42,24 @@ class Solution:
 						heapq.heappush(deq, (newCost, newY, newX, 1, ndy, ndx))
 		return 0
 
-	def dijkstra2(self):
+	def part2_dijkstra(self):
 		visited = set()
 		deq = []
-		heapq.heappush(deq, (0, 0, 0, 1, 0, 1)) # cost, y, x, steps, dirY, dirX
-		heapq.heappush(deq, (0, 0, 0, 1, 1, 0))
+		heapq.heappush(deq, (0, 0, 0, 0, 0, 1)) # cost, y, x, steps, dirY, dirX
+		heapq.heappush(deq, (0, 0, 0, 0, 1, 0))
 
 		while deq:
 			cost, y, x, s, dy, dx = heapq.heappop(deq)
 
-			if s == 9:
-				continue
-
 			if (y, x, dy, dx, s) in visited:
 				continue
 			visited.add((y, x, dy, dx, s))
-			if cost < self.grid[y][x]:
-				self.grid[y][x] = cost
 
+			# found end
 			if y == self.lY - 1  and x == self.lX - 1:
 				return cost
 			
-			for ndy, ndx in self.dir:
-				
+			for ndy, ndx in self.dir:	
 				if ndy == -dy and ndx == -dx:
 					continue
 
@@ -83,30 +68,19 @@ class Solution:
 
 				if 0 <= newY < self.lY and 0 <= newX < self.lX:
 					newCost = cost + int(self.map[newY][newX])
-
 					if [dy,dx] == [ndy,ndx]:
-						heapq.heappush(deq, (newCost, newY, newX, s + 1, ndy, ndx))
-					elif s > 3:
+						if s < 10:
+							heapq.heappush(deq, (newCost, newY, newX, s + 1, ndy, ndx))
+					elif s >= 4:
 						heapq.heappush(deq, (newCost, newY, newX, 1, ndy, ndx))
 		return 0
 					
-
-	def part1(self):
-		return self.dijkstra()
-
-	def part2(self):
-		total = self.dijkstra2()
-		for line in self.grid:
-			print(line)
-		return total
-
 def main():
-	# CHANGE INPUTFILE
 	input = open("input/17.txt", "r").read()
 
 	sol = Solution(input)
-	print("Part 1:", sol.part1())
-	print("Part 2:", sol.part2())
+	print("Part 1:", sol.part1_dijkstra())
+	print("Part 2:", sol.part2_dijkstra())
 
 if __name__ == "__main__":
 	main()
