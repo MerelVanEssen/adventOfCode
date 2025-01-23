f6330ad939ad6caccd987f452603b5e6441ec504
Merel <mvan-ess@codam.student.nl>
2025-01-23 07:37:22 +0100

    07 2022

--- 2022/07.py
+++ 2022/07.py
@@ -1,80 +1,47 @@
-from collections import Counter
+from collections import defaultdict
 
 with open("input/07.txt", "r") as f:
 	rawInput = f.read()
 
 commands = rawInput.split('\n')
 
-class Folder:
-	def __init__(self, name, childs, files, parent):
-		self.name = name
-		self.childs = childs
-		self.files = files
-		self.parent = parent
-
-		def __lt__(self, other):
-			return self.depth < other.depth
-
 folders = []
-all = []
-start = False
-for item in commands:
-	if item[:4] == "$ cd":
+dic = defaultdict(int)
 
-		if start == True:
-			if len(folders) > 1:
-				all.append(Folder(folders[-1], childs, files, folders[-2]))
-			else:
-				all.append(Folder(folders[-1], childs, files, folders[-1]))
-
-		name = item[5:]
+for command in commands:
+	if command[:4] == "$ cd":
+		name = command[5:]
 		if name == "..":
 			folders.pop()
+		elif name == "/":
+			folders = ["/"]
 		else:
 			folders.append(name)
-		start = False
-	elif item[:4] == "$ ls":
-		childs = []
-		files = []
-		start = True
-	elif item[0].isdigit():
-		size, file = item.split()
-		files.append((file, int(size)))
+	elif command[:4] == "$ ls":
+		continue
 	else:
-		childs.append(item[4:])
-
-if len(folders) > 1:
-	all.append(Folder(folders[-1], childs, files, folders[-2]))
-else:
-	all.append(Folder(folders[-1], childs, files, folders[-1]))
-
-def getSize(folder, all):
-
-	size = 0
-
-	for file, s in folder.files:
-		size += s
-	
-	for dir in folder.childs:
-		for searchFolder in all:
-			if searchFolder.name == dir and searchFolder.parent == folder.name:
-				size += getSize(searchFolder, all)
-
-	return size
-
+		value, file = command.split()
+		if value != "dir":
+			size = int(value)
+			for i, folder in enumerate(folders):
+				path = "/".join(folders[:i + 1])
+				dic[path] += int(value)
 
 total = 0
-for folder in all:
-	print(folder.parent, folder.name, folder.childs, folder.files)
-	size = getSize(folder, all)
-	print(size)
-	if size < 100000:
-		total += size
+removed_max = float('inf')
+folder_size = 0
+size_needed = 30000000 - (70000000 - dic["/"])
+
+for name, value in dic.items():
+	if value <= 100000:
+		total += value
+	if value > size_needed:
+		if value - size_needed < removed_max:
+			folder_size = value
+			removed_max = value - size_needed
+	
+print("Part 1:", total)
+print("Part 2:", folder_size)
 
 
 
-
-
-print("Part 1:", total, 1297683)
-
-# 813094 too low
